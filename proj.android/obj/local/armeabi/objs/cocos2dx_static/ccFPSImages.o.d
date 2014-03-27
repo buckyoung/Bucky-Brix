24938d15337ec763341bb807e3397dea7af6df82
professir <buckcyoung@gmail.com>
2014-03-27 13:35:35 -0400

    rm

--- proj.android/obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o: \
- /Users/BuckYoung/cocos2d-x-2.1.5/HelloWorld/proj.android/../../cocos2dx/ccFPSImages.c \
- /Users/BuckYoung/cocos2d-x-2.1.5/HelloWorld/proj.android/../../cocos2dx/ccFPSImages.h
-
-/Users/BuckYoung/cocos2d-x-2.1.5/HelloWorld/proj.android/../../cocos2dx/ccFPSImages.h:
--- proj.android/obj/local/armeabi/objs/cocos2dx_static/kazmath/src/neon_matrix_impl.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/kazmath/src/neon_matrix_impl.o: \
- /Users/BuckYoung/cocos2d-x-2.1.5/HelloWorld/proj.android/../../cocos2dx/kazmath/src/neon_matrix_impl.c \
- /Users/BuckYoung/cocos2d-x-2.1.5/HelloWorld/proj.android/../../cocos2dx/kazmath/include/kazmath/neon_matrix_impl.h
-
-/Users/BuckYoung/cocos2d-x-2.1.5/HelloWorld/proj.android/../../cocos2dx/kazmath/include/kazmath/neon_matrix_impl.h:
